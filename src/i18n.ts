5fb3d1776ea9920a0a109617c1ce7aadbe084c4d
Mirac <miracerdin@gmail.com>
2022-10-12 13:05:28 +0300

    i18n changed

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,27 +1,28 @@
 import Vue from "vue";
 import VueI18n, { LocaleMessages } from "vue-i18n";
-
+import en from "./locales/en";
+import tr from "./locales/tr";
 Vue.use(VueI18n);
 
-function loadLocaleMessages(): LocaleMessages {
-  const locales = require.context(
-    "./locales",
-    true,
-    /[A-Za-z0-9-_,\s]+\.json$/i
-  );
-  const messages: LocaleMessages = {};
-  locales.keys().forEach((key) => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
-    }
-  });
-  return messages;
-}
+// function loadLocaleMessages(): LocaleMessages {
+//   const locales = require.context(
+//     "./locales",
+//     true,
+//     /[A-Za-z0-9-_,\s]+\.json$/i
+//   );
+//   const messages: LocaleMessages = {};
+//   locales.keys().forEach((key) => {
+//     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+//     if (matched && matched.length > 1) {
+//       const locale = matched[1];
+//       messages[locale] = locales(key).default;
+//     }
+//   });
+//   return messages;
+// }
 
 export default new VueI18n({
-  locale: "en",
-  fallbackLocale: "en",
-  messages: loadLocaleMessages(),
+  locale: localStorage.getItem("lang") || "en",
+  fallbackLocale: localStorage.getItem("lang") || "en",
+  messages: { en, tr },
 });
