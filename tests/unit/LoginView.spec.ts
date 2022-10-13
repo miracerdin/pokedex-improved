4019e0bcabbb7a4d4dabfa194fa198ef4bb81e05
Mirac <miracerdin@gmail.com>
2022-10-13 16:03:28 +0300

    loginPage test

--- tests/unit/LoginView.spec.ts
+++ tests/unit/LoginView.spec.ts
@@ -1,11 +1,27 @@
 import LoginView from "@/views/LoginView.vue";
 import i18n from "@/i18n";
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import {
+  shallowMount,
+  createLocalVue,
+  mount,
+  RouterLinkStub,
+} from "@vue/test-utils";
 import VueI18n from "vue-i18n";
+import VueRouter from "vue-router";
+import Vue from "vue";
 
 const localVue = createLocalVue();
-localVue.use(VueI18n);
+// const routerVue = createLocalVue();
+localVue.use(VueI18n, VueRouter);
+// routerVue.use(VueRouter);
+// const router = new VueRouter();
+
+// Vue.use(VueRouter);
+
 describe("LoginView.vue", () => {
+  //   beforeEach(() => {
+  //     const wrapper = shallowMount(LoginView, { router });
+  //   });
   it("renders the component", () => {
     const wrapper = shallowMount(LoginView, {
       localVue,
@@ -39,5 +55,18 @@ describe("LoginView.vue", () => {
       wrapper.vm.$nextTick();
       expect(window.alert).toBeCalled();
     }
+  });
+
+  it("renders a child component via routing", async () => {
+    const wrapper = shallowMount(LoginView, {
+      localVue,
+      i18n,
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toBe(
+      "/en/RegisterView"
+    );
   });
 });
