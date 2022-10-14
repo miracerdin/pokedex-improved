35ff399d6b6c9197eb95de900d1ba6f1b3dc8f3f
Mirac <miracerdin@gmail.com>
2022-10-14 10:16:28 +0300

    local storage mocked

--- tests/unit/LanguageSwitcher.spec.ts
+++ tests/unit/LanguageSwitcher.spec.ts
@@ -2,6 +2,48 @@ import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
 import { shallowMount } from "@vue/test-utils";
 
 describe("LanguageSwitcher", () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  const localStorageMock = (function () {
+    interface Local {
+      [key: string]: string; // key ve value ya type vermem gerekiyordu and i used an interface [key:string] and value :string. if value was a number type [key:string]:number would be.
+    }
+
+    let store: Local = {};
+
+    return {
+      getItem(key: string) {
+        return store[key];
+      },
+
+      setItem(key: string, value: string) {
+        store[key] = value;
+      },
+
+      clear() {
+        store = {};
+      },
+
+      removeItem(key: string) {
+        delete store[key];
+      },
+
+      getAll() {
+        return store;
+      },
+    };
+  })();
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
+  const setLocalStorage = (id: string, data: any) => {
+    window.localStorage.setItem(id, JSON.stringify(data));
+  };
+  it("when the page landed there should be an empty array with liste key", () => {
+    setLocalStorage("lang", "en");
+    expect(window.localStorage.getItem("lang")).toBe('"en"');
+  });
   test("renders the header component", () => {
     const wrapper = shallowMount(LanguageSwitcher);
     expect(wrapper.find(".container").exists()).toBe(true);
